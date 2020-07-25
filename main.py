035d442d5ec768461c108c7f40c8f332d7a509dd
aaawheel <acwheel@hotmail.com>
2020-07-25 21:48:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+spells = {
+  'lvl_1': ['create_water', 'cure_minor_wounds', 'detect_magic', 'detect_poison', 'guidance', 'inflict_minor_wounds', 'light', 'mending', 'purify_food_drink', 'read_magic', 'resistance', 'virtue'],
+
+  'lvl_2':['aid', 'align_weapon', 'augury', 'bears_endurance', 'bulls_strength', 'calm_emotions', 'consecrate', 'cure_moderate_wounds', 'darkness', 'death_knell', 'delay_poison', 'desecrate', 'eagles_splendor', 'enthrall', 'find_traps', 'gentle_repose', 'hold_person', 'inflict_moderate_wounds', 'make_whole', 'owls_wisdom', 'remove_paralysis', 'resist_energy', 'restoration_lesser', 'shatter', 'shield_other', 'silence', 'sound_burst', 'spiritual_weapon', 'status', 'summon_monster_2', 'undetectable_alignment', 'zone_of_truth']}
+
+slots = {
+  'slot_lvl_0': 6,
+  'slot_lvl_1': 6,
+  'slot_lvl_2': 5,
+  'slot_lvl_3': 5,
+  'slot_lvl_4': 4,
+  'slot_lvl_5': 3,
+  'slot_lvl_6': 2}
+#format to print the list in the dict
+#print(spells['lvl_1'])
+
+#format to print items from the list in the dict on their own lines, in order
+#print("Level One:\n")
+#for item in spells['lvl_1']:
+#  print (item)
+
+#find a better way to do the new line
+#print("\n")
+#print("Level Two:\n")
+#for item in spells['lvl_2']:
+#  print(item)
+
